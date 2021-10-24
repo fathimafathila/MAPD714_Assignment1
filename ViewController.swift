a8721f127163ca678e58c4047d73ee4d101df54f
fathimafathila <fathima.fathila1997@gmail.com>
2021-10-24 17:27:37 -0400

    Initial Commit + Added Functionalities for Number, Operation, Special buttons pressed

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MAPD714_Assignment1
+//
+//  Created by fathila on 2021-10-22.
+//
+/*
+ Author     : Fathima Fathila
+ Student Id : 301222885
+ Date       : oct 22 2021s
+ */
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
